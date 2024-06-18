00bc654aac9e97d211ecaebf156933060ddf56d8
AnuragDahal <freefirepros2020@gmail.com>
2024-06-18 13:10:55 +0545

    feat:Added side navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/shadcn-ui/Button";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default App;
