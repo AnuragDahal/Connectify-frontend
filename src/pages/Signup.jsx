8b79ed881de70c5e948e7f6177f83bb3805abc02
AnuragDahal <freefirepros2020@gmail.com>
2024-06-21 18:16:35 +0545

    feat: Update Login page layout and add Google login option

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
-import { TEInput, TERipple } from "tw-elements-react";
 
 const SignUp = () => {
   return (
     <>
       <section className="mt-10">
-        <div className="container h-full px-6 py-24">
+        <div className="container h-full gap-30 px-6 py-24">
           <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
             {/* <!-- Left column container with background--> */}
             <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
@@ -18,36 +17,35 @@ const SignUp = () => {
             {/* <!-- Right column container with form --> */}
             <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
               <header className="flex-col justify-start">
-                <h1 className="text-blue-500 font-extrabold text-6xl">
+                <h1 className="text-blue-500 flex justify-center font-extrabold text-6xl">
                   CONNECTIFY
                 </h1>
-                <span className="text-xl mb-10 flex justify-start font-serif font-medium">
+                <span className="text-xl mb-10 flex justify-center font-serif font-medium">
                   Welcome to Connectify
                 </span>
               </header>
-              <form>
+              <form className="flex flex-col ">
                 <label>Name</label>
-                <TEInput
+                <input
                   type="text"
                   placeholder="Enter your Name"
                   size="lg"
-                  className="mb-6 bg-zinc-300"
-                ></TEInput>
+                  className="mb-6 font-sans font-light"
+                ></input>
                 <label>Email address</label>
-                <TEInput
+                <input
                   type="email"
                   placeholder="Enter your email address"
                   size="lg"
-                  className="mb-6  bg-zinc-300"
-                ></TEInput>
-
+                  className="mb-6"
+                ></input>
                 <label>Password</label>
-                <TEInput
+                <input
                   type="password"
                   placeholder="Enter your password"
-                  className="mb-6"
-                  size="lg  bg-zinc-300"
-                ></TEInput>
+                  className="mb-6 bg-zinc-300"
+                  size="lg"
+                ></input>
                 <div className="mb-6 flex items-center justify-between">
                   <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
                     <input
@@ -61,18 +59,16 @@ const SignUp = () => {
 
                   {/* <!-- Forgot password link --> */}
                 </div>
-
                 {/* <!-- Submit button --> */}
-
-                <TERipple rippleColor="light" className="w-full">
+                <div className="w-full">
                   <button
                     type="button"
                     className="inline-block w-full rounded bg-primary px-7 pb-2.5 bg-blue-400 hover:bg-blue-500 pt-3 text-sm font-semibold uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                   >
                     Sign up
                   </button>
-                </TERipple>
-                <TERipple rippleColor="light" className="w-full">
+                </div>
+                <div className="w-full">
                   <span className="font-lighter text-black my-3 flex justify-center">
                     Already Have An Account?
                   </span>
@@ -84,17 +80,15 @@ const SignUp = () => {
                       Login
                     </button>
                   </Link>
-                </TERipple>
-
+                </div>
                 {/* <!-- Divider --> */}
                 <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
                   <p className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
                     OR
                   </p>
                 </div>
-
                 {/* <!-- Social login buttons --> */}
-                <TERipple rippleColor="light" className="w-full">
+                <div className="w-full">
                   <a
                     className="mb-3 flex w-full items-center justify-center rounded bg-primary px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                     style={{ backgroundColor: "#3b5998" }}
@@ -103,7 +97,6 @@ const SignUp = () => {
                   >
                     {/* <!-- Facebook --> */}
                     <svg
-                      xmlns="http://www.w3.org/2000/svg"
                       className="mr-2 h-3.5 w-3.5"
                       fill="currentColor"
                       viewBox="0 0 24 24"
@@ -112,26 +105,7 @@ const SignUp = () => {
                     </svg>
                     Continue with Facebook
                   </a>
-                </TERipple>
-                <TERipple rippleColor="light" className="w-full">
-                  <a
-                    className="mb-3 flex w-full items-center justify-center rounded bg-info px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]"
-                    style={{ backgroundColor: "#55acee" }}
-                    href="#!"
-                    role="button"
-                  >
-                    {/* <!-- Twitter --> */}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="mr-2 h-3.5 w-3.5"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-                    </svg>
-                    Continue with Twitter
-                  </a>
-                </TERipple>
+                </div>
               </form>
             </div>
           </div>
