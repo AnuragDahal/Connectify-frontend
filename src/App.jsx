7cd7e86424f2d2f5b5a1ebed7418a828f4afcd90
AnuragDahal <freefirepros2020@gmail.com>
2024-06-22 23:05:49 +0545

    feat: Update NavBar component and page layouts

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
 import { Route, Routes, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
+import Home from "./components/SideBar";
 
-const PATH_NOT_TO_RENDER_NAVBAR = ["/login", "/signup"];
+const PATH_NOT_TO_RENDER_NAVBAR_AND_SIDEBAR = ["/login", "/signup"];
 
 const App = () => {
   const location = useLocation();
   return (
     <>
-      {PATH_NOT_TO_RENDER_NAVBAR.includes(location.pathname) ? null : (
-        <NavBar />
+      {PATH_NOT_TO_RENDER_NAVBAR_AND_SIDEBAR.includes(
+        location.pathname
+      ) ? null : (
+        <>
+          <NavBar />
+          <SideBar />
+        </>
       )}
       <Routes>
         <Route path="/" element={<Home />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,55 +1,5 @@
-import { AiFillHome } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { IoNotifications, IoSettingsSharp } from "react-icons/io5";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "../components/Accordion";
-
-const Home = () => {
-  return (
-    <>
-      <div className="my-14 h-screen w-52 bg-gray-400">
-        <ul className="flex flex-col gap-6 text-white font-sans font-10 font-semibold fixed px-5">
-          <li className="hover:cursor-pointer text-lg font-medium font-serif">
-            <Link
-              className="flex mt-10 align-baseline gap-1"
-              to="/"
-            >
-              <AiFillHome className="hover:cursor-pointer" />
-              Home
-            </Link>
-          </li>
-          <li className="hover:cursor-pointer hover:underline text-lg font-medium font-serif">
-            <Link to="/about">About</Link>
-          </li>
-          <li className="hover:cursor-pointer hover:underline text-lg font-medium font-serif">
-            <Link
-              to="/notifications"
-              className="flex flex-wrap items-center justify-center gap-2 "
-            >
-              <IoNotifications className="hover:cursor-pointer" />
-              Notifications
-            </Link>
-          </li>
-          <li>
-            <Accordion type="single" collapsible className="w-full">
-              <AccordionItem value="item-1">
-                <AccordionTrigger>
-                  <IoSettingsSharp />
-                  Settings
-                </AccordionTrigger>
-                <AccordionContent>Change password</AccordionContent>
-                <AccordionContent>Verify Email</AccordionContent>
-                <AccordionContent>Logout</AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+const Layout = () => {
+  return <></>;
 };
-export default Home;
+
+export default Layout;
