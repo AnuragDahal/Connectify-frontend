78621f2e6ac37cf70737c0620764994698a658ef
AnuragDahal <freefirepros2020@gmail.com>
2024-06-23 12:42:38 +0545

    feat: Add card component to Home page for creating projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,41 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/PostCard";
+
 const Home = () => {
   return (
     <>
       <div className="pt-14 h-screen">
-        <h1 className="text-4xl font-bold text-black">
-          Welcome to the Home Page
-        </h1>
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>
+              Deploy your new project in one-click.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Title</Label>
+                  <Input id="name" placeholder="Title of Post" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="framework">Framework</Label>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline">Cancel</Button>
+            <Button>Post</Button>
+          </CardFooter>
+        </Card>
       </div>
     </>
   );
