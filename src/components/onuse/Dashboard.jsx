9d11683fcc8f7f2116596c9498464dc66eb2263f
AnuragDahal <freefirepros2020@gmail.com>
2024-06-27 18:02:44 +0545

    feat; added postcard and props

--- src/components/onuse/Dashboard.jsx
+++ src/components/onuse/Dashboard.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import NavBar from "../NavBar";
 import SideBar from "../SideBar";
-import CreatePost from "../../pages/CreatePost";
+import Post from "../Post";
 
 const Dashboard = () => {
+  const data = [
+    {
+      fullName: "Anurag Dahal",
+      description: "This is a description",
+      imageLink:
+        "https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      fullName: "Salman khan",
+      description: "This is a description",
+      imageLink:
+        "https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+  ];
   return (
     <>
       <NavBar />
-      <div className="flex flex-row "></div>
-      <SideBar />
+      <div className="flex h-auto flex-row gap-30">
+        <SideBar />
+        <div className="flex px-40 w-full flex-col items-center justify-center ">
+          {data.map((item) => (
+            <Post {...item} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
