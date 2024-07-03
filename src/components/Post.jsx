2f1908a60c53dc232b7ff09896e969617ecc9069
AnuragDahal <freefirepros2020@gmail.com>
2024-07-03 23:24:03 +0545

    refactor: Improve Post component readability and add new props

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-const Post = ({ title, content, posted_by, images }) => {
+const Post = ({ title, content, posted_by, images, likes, comments }) => {
   return (
     <>
       <div className="flex bg-white max-h-svh shadow-lg rounded-lg mx-4 md:mx-auto max-w-md md:max-w-2xl ">
@@ -22,7 +22,7 @@ const Post = ({ title, content, posted_by, images }) => {
                 src={images[0]}
               />
             </div>
-            <div className="mt-4 flex items-center">
+            <div className="mt-4 flex justify-around items-center">
               <div className="flex mr-2 text-gray-700 text-sm ">
                 <svg
                   fill="none"
@@ -37,9 +37,9 @@ const Post = ({ title, content, posted_by, images }) => {
                     d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                   />
                 </svg>
-                <span>12</span>
+                <span>{likes.length}</span>
               </div>
-              <div className="flex text-gray-700 text-sm mr-8">
+              <div className="flex text-gray-700 text-sm">
                 <svg
                   fill="none"
                   viewBox="0 0 24 24"
@@ -53,7 +53,7 @@ const Post = ({ title, content, posted_by, images }) => {
                     d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
                   />
                 </svg>
-                <span>8</span>
+                <span>{comments.length}</span>
               </div>
               <div className="flex text-gray-700 text-sm mr-4">
                 <svg
@@ -69,7 +69,7 @@ const Post = ({ title, content, posted_by, images }) => {
                     d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
                   />
                 </svg>
-                <span>share</span>
+                <span className="font-medium">share</span>
               </div>
             </div>
           </div>
