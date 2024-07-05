ae3fae26bc2ae9dffdf643538643127f1cfad2b3
AnuragDahal <freefirepros2020@gmail.com>
2024-07-05 23:41:23 +0545

    feat: Add embla-carousel-react and lucide dependencies

--- src/components/onuse/Dashboard.jsx
+++ src/components/onuse/Dashboard.jsx
@@ -4,6 +4,15 @@ import SideBar from "../SideBar";
 import Post from "../Post";
 import { useAuth } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { CirclePlus, Paperclip } from "lucide-react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/Carousel";
+
 import {
   Dialog,
   DialogTrigger,
@@ -16,8 +25,14 @@ const Dashboard = () => {
   const navigate = useNavigate();
   const { token } = useAuth();
   const [title, setTitle] = useState("");
-  const [images, setImages] = useState([]);
+  // const [titleWordCount, setTitleWordCount] = useState(0);
+  // const [isTitleFocused, setIsTitleFocused] = useState(false);
+  const [isContentFocused, setIsContentFocused] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
   const [content, setContent] = useState("");
+  const [images, setImages] = useState([]);
+  const [preview, setPreview] = useState([]);
+
   const fd = new FormData();
   fd.append("title", title);
   fd.append("content", content);
@@ -32,7 +47,7 @@ const Dashboard = () => {
       body: fd,
     })
       .then((res) => {
-        if (res.status == 201) {
+        if (res.ok == 201) {
           navigate("/");
         }
       })
@@ -49,9 +64,8 @@ const Dashboard = () => {
     })
       .then((response) => response.json())
       .then((fetchedData) => {
-        // Ensure fetchedData is an array before setting it
         if (Array.isArray(fetchedData)) {
-          data.push(setData(fetchedData));
+          setData(fetchedData);
         } else {
           console.error("Fetched data is not an array:", fetchedData);
         }
@@ -59,6 +73,38 @@ const Dashboard = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, [token]);
 
+  const handleImageChange = (event) => {
+    const files = Array.from(event.target.files).map((file) => ({
+      url: URL.createObjectURL(file),
+      name: file.name,
+      preview: ["jpg", "jpeg", "png", "gif"].includes(
+        file.name.split(".").pop().toLowerCase()
+      ),
+      size:
+        file.size > 1024
+          ? file.size > 1048576
+            ? Math.round(file.size / 1048576) + "mb"
+            : Math.round(file.size / 1024) + "kb"
+          : file.size + "b",
+    }));
+    setPreview(files);
+    setImages(Array.from(event.target.files));
+  };
+
+  const removeImage = (index) => {
+    setImages(images.filter((_, i) => i !== index));
+  };
+
+  const handleContentChange = (e) => {
+    const inputValue = e.target.value;
+    if (inputValue.length <= 300) {
+      setContent(inputValue);
+      setWordCount(inputValue.length);
+    } else {
+      setContent(inputValue.substring(0, 300));
+      setWordCount(300);
+    }
+  };
   return (
     <>
       <NavBar />
@@ -66,76 +112,106 @@ const Dashboard = () => {
         <SideBar />
 
         <div className="flex px-40 w-full flex-col items-center pt-20 gap-7 justify-center pb-4">
-          <Dialog className="bg-black text-white">
-            <DialogTrigger className="bg-black text-white rounded-md p-2 font-sans font-semibold">
-              Add New Post
+          <Dialog>
+            <DialogTrigger className="bg-violet-600 flex items-center gap-2 text-white rounded-md p-2 font-sans font-semibold">
+              <CirclePlus /> Add Post
             </DialogTrigger>
             <DialogTitle></DialogTitle>
-            <DialogContent className="bg-black text-white h-auto">
-              <DialogDescription></DialogDescription>
-              <div className="w-full">
-                <div>
-                  <h1>New Post</h1>
-                </div>
-                <div className="mt-2">
-                  <form>
-                    <div className="grid w-full items-center gap-4">
-                      <div className="flex flex-col space-y-1.5">
-                        <label htmlFor="name">Title</label>
+            <DialogContent className="bg-white">
+              <DialogDescription>
+                <div className="p-4 py-8">
+                  <div className="heading text-center font-bold text-2xl m-5 text-gray-800">
+                    New Post
+                  </div>
+                  <div className="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 max-w-2xl">
+                    <input
+                      id="title"
+                      className="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none"
+                      spellCheck="false"
+                      placeholder="Title"
+                      type="text"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                      onFocus={() => setIsTitleFocused(true)}
+                      onBlur={() => setIsTitleFocused(false)}
+                    />
+                    <textarea
+                      className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none"
+                      spellCheck="false"
+                      placeholder="Describe everything about this post here"
+                      onChange={handleContentChange}
+                      onFocus={() => setIsContentFocused(true)}
+                      onBlur={() => setIsContentFocused(false)}
+                      value={content}
+                    ></textarea>
+
+                    <div className="icons flex text-gray-500 m-2">
+                      <label htmlFor="select-image" className="cursor-pointer">
                         <input
-                          id="name"
-                          className="text-black font-medium font-sans"
-                          value={title}
-                          onChange={(e) => {
-                            setTitle(e.target.value);
-                          }}
-                          placeholder="Title of Post"
-                        />
-                        <label htmlFor="name">Description</label>
-                        <input
-                          className="text-black font-light font-sans"
-                          id="name"
-                          value={content}
-                          onChange={(e) => {
-                            setContent(e.target.value);
-                          }}
-                          placeholder="Content of Post"
-                        />
-                        <input
-                          id="imageupload"
+                          id="select-image"
                           type="file"
-                          name="images"
                           accept="image/*"
                           multiple
-                          onChange={(e) => {
-                            const files = Array.from(e.target.files);
-                            const imageUrls = files.map((file) =>
-                              URL.createObjectURL(file)
-                            );
-                            setLoadImg(imageUrls);
-                            setImages([...e.target.files]);
-                          }}
+                          hidden
+                          onChange={handleImageChange}
                         />
+                        <Paperclip />
+                      </label>
+                      <div className="count ml-auto text-gray-400 text-xs font-semibold">
+                        {isContentFocused ? `${wordCount}/300` : null}
                       </div>
                     </div>
-                  </form>
+                    {/* <Carousel>
+                      <CarouselContent>
+                        <CarouselItem>...</CarouselItem>
+                        <CarouselItem>...</CarouselItem>
+                        <CarouselItem>...</CarouselItem>
+                      </CarouselContent>
+                      <CarouselPrevious />
+                      <CarouselNext />
+                    </Carousel> */}
+
+                    <div id="preview" className="my-4  overflow-x-scroll flex">
+                      {preview.map((image, index) => (
+                        <div
+                          key={index}
+                          className="relative w-32 h-32 object-cover rounded"
+                        >
+                          {image.preview && (
+                            <div className="relative w-32 h-32 object-cover rounded">
+                              <img
+                                src={image.url}
+                                alt=""
+                                className="w-32 h-32 object-cover rounded"
+                              />
+                              <button
+                                onClick={() => removeImage(index)}
+                                className="w-6 h-6 absolute text-center flex items-center top-0 right-0 m-2 text-white text-lg bg-red-500 hover:text-red-700 hover:bg-gray-100 rounded-full p-1"
+                              >
+                                ×
+                              </button>
+                              <div className="text-xs text-center p-2">
+                                {image.size}
+                              </div>
+                            </div>
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                    <div className="buttons flex justify-end">
+                      <button
+                        onClick={handlePostCreate}
+                        className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500"
+                      >
+                        Post
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex justify-between">
-                  <button className="bg-white text-black hover:drop-shadow-md mt-2 p-2 rounded-md hover:cursor-pointer">
-                    Cancel
-                  </button>
-                  <button
-                    className="bg-white text-black hover:drop-shadow-md mt-2 p-2 rounded-md hover:cursor-pointer"
-                    onClick={handlePostCreate}
-                  >
-                    Post
-                  </button>
-                </div>
-              </div>
+              </DialogDescription>
             </DialogContent>
           </Dialog>
-
-          <div className="">
+          <div>
             {data.map((item, index) => (
               <Post key={index} {...item} />
             ))}
@@ -145,5 +221,4 @@ const Dashboard = () => {
     </>
   );
 };
-
 export default Dashboard;
